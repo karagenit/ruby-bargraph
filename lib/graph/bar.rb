34a1d12458a79c9ef86f9d7935cf02a1e1f6c2d9
Caleb Smith <karagenit@outlook.com>
2017-09-20 21:57:25 -0400

    Add RDoc to Library

--- lib/graph/bar.rb
+++ lib/graph/bar.rb
@@ -1,14 +1,29 @@
+##
+# This module contains tools for analyzing and graphing data arrays
+#
 module Graph
+  ##
+  # This class allows the display of data arrays in bar graph format
+  #
   class Bar
+    ##
+    # Allows optionally setting the initial dataset as a parameter
+    #
     def initialize(data = nil)
       @data = data
     end
 
+    ##
+    # Get or Set the data array
+    #
     def data(data = nil)
       @data = data unless data.nil?
       @data
     end
 
+    ##
+    # Print the bar graph to stdout
+    #
     def print
       @data.each_with_index do |count, index|
         printf "%02d|%s\n", index, ('#' * count)
